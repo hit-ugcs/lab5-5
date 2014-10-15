c6fffe520d8ad83d1bdb7495737606356ee80c30
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-10-15 19:02:55 +0800

    test

--- README.md
+++
@@ -1,5 +0,0 @@
-HIT SaaS Course Lab 3: Grading Sheet
-====================================
-
-+ A simple grading sheet app to practice Ruby on Rails
-
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lab3::Application.config.secret_token = 'a77e9b64a11d20f2323106b2640fb9752f4f1f971ed6dded8c3d1ad3fd45b36770a5b43dcfc648bce38e686061a10e6ed6879388641c48a7f9e1d4764ed1c71e'
