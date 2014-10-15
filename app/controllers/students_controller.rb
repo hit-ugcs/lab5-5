f1f19fd032798b7fc2a48c3fe93338d0c7b6de53
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-10-15 20:38:34 +0800

    added CSS style; changed maximum rows to 10

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   # GET /students.json
 
   def index
-    @students = Student.paginate :page => params[:page], :per_page => 2
+    @students = Student.paginate :page => params[:page], :per_page => 10
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @students }
