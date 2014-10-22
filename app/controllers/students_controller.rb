52965afb12cc65714f69e67be54eeb89d9d29795
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-10-22 19:37:50 +0800

    fixed pagination bugs in printing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,7 @@ class StudentsController < ApplicationController
   # GET /students.json
 
   def index
+    @students_print = Student.all
     @students = Student.paginate :page => params[:page], :per_page => 10
     respond_to do |format|
       format.html # index.html.erb
