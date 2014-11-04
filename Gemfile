32b6fcb75d54221d34928c39135b55aa46699d5b
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-11-04 22:37:59 +0800

    updated to rails 4

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-gem 'sqlite3'
+# gem 'sqlite3'
 gem 'devise'
 gem 'protected_attributes'
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,32 +9,35 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141104120121) do
+ActiveRecord::Schema.define(version: 20141104120121) do
 
-  create_table "discussions", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "discussions", force: true do |t|
     t.integer  "student_id"
     t.integer  "score"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "labs", :force => true do |t|
+  create_table "labs", force: true do |t|
     t.integer  "student_id"
     t.integer  "score"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "projects", :force => true do |t|
+  create_table "projects", force: true do |t|
     t.integer  "student_id"
     t.integer  "score"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "students", :force => true do |t|
+  create_table "students", force: true do |t|
     t.string   "student_id"
     t.string   "name"
     t.string   "nickname"
@@ -43,8 +46,8 @@ ActiveRecord::Schema.define(:version => 20141104120121) do
     t.integer  "lab"
     t.integer  "classdiscussion"
     t.integer  "total"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "classdiscussion1"
     t.integer  "classdiscussion2"
     t.integer  "classdiscussion3"
@@ -68,22 +71,22 @@ ActiveRecord::Schema.define(:version => 20141104120121) do
     t.integer  "project3"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
