9aa963f8a771e9e230b97fd759132c057abe7661
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-11-06 13:16:39 +0800

    database splitted

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
 source 'https://rubygems.org'
 
 ruby '1.9.3'
-gem 'pg'
 gem 'rails'
-
 gem 'devise'
 gem 'protected_attributes'
 gem 'rails_12factor'
@@ -17,6 +15,10 @@ gem 'will_paginate'
 gem 'bootstrap-will_paginate'
 group :production do
   gem 'thin'
+  gem 'pg'
+end
+group :development do
+  gem 'sqlite3'
 end
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', :platforms => :ruby
--- db/schema.rb
+++ db/schema.rb
@@ -13,30 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141104120121) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "discussions", force: true do |t|
-    t.integer  "student_id"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "labs", force: true do |t|
-    t.integer  "student_id"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "projects", force: true do |t|
-    t.integer  "student_id"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "students", force: true do |t|
     t.string   "student_id"
     t.string   "name"
@@ -86,7 +62,7 @@ ActiveRecord::Schema.define(version: 20141104120121) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
